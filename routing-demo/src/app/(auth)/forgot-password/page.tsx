1164a45ea40540fdf4c98929991ce209c25d241c
sangamSanjeet <factsbe0@gmail.com>
2024-05-01 13:43:18 +0530

    grouping routes, private folders

---
+++ routing-demo/src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,5 @@
+const ForgotPassword = () => {
+	return <div>Password</div>;
+};
+
+export default ForgotPassword;
---
+++ routing-demo/src/app/_lib/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+	return <div>THIS IS PRIVATE</div>;
+};
+
+export default page;
