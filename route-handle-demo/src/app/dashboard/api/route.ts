9e2519d2301ffc28defd0a5522b04164c81390f4
sangamSanjeet <factsbe0@gmail.com>
2024-05-03 22:29:13 +0530

    Cookies in Route Handlers

--- route-handle-demo/src/app/dashboard/api/route.ts
+++ route-handle-demo/src/app/dashboard/api/route.ts
@@ -1,14 +1,23 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-	const requestHeader = new Headers(request.headers);
-	const headersList = headers();
-	console.log(requestHeader.get("Authorization"));
-	console.log(headersList.get("Authorization"));
+	// const requestHeader = new Headers(request.headers);
+	// const headersList = headers();
+
+	// console.log(requestHeader.get("Authorization"));
+	// console.log(headersList.get("Authorization"));
+
+	cookies().set("resultsPerPage", "20");
+
+	const theme = request.cookies.get("theme");
+	console.log(theme);
+	console.log(cookies().get("resultsPerPage"));
+
 	return new Response("<h1>Dashboard API Documentation</h1>", {
 		headers: {
 			"Content-Type": "text/html",
+			"Set-Cookie": "theme=dark",
 		},
 	});
 }
