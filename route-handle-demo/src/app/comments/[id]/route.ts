ce9ad0aca1c49c25476ad47b85609612fca1fa09
sangamSanjeet <factsbe0@gmail.com>
2024-05-03 21:44:56 +0530

    route-handler_CRUD with route.ts

---
+++ route-handle-demo/src/app/comments/[id]/route.ts
@@ -0,0 +1,41 @@
+import { comments } from "../data";
+
+export async function GET(_response: Request, { params }: { params: { id: string } }) {
+	const comment = comments.find((comment) => comment.id === parseInt(params.id));
+	return new Response(JSON.stringify(comment), {
+		headers: {
+			"Content-Type": "application/json",
+		},
+		status: 200,
+	});
+}
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+	const body = await request.json();
+	const { text } = body;
+	const index = comments.findIndex((comment) => comment.id === parseInt(params.id));
+	if (index === -1) {
+		return new Response("No comments found");
+	}
+
+	const comment = comments[index];
+	comment.text = text;
+
+	return new Response(JSON.stringify(comment), {
+		headers: {
+			"Content-Type": "application/json",
+		},
+		status: 200,
+	});
+}
+
+export async function DELETE(_request: Request, { params }: { params: { id: string } }) {
+	const index = comments.findIndex((comment) => comment.id === parseInt(params.id));
+	if (index === -1) {
+		return new Response("No comments found");
+	}
+
+	comments.splice(index, 1);
+
+	return new Response("Comment deleted");
+}
