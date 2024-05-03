a00786800ee10740db6e8bfea06d36e0f7bde3e8
sangamSanjeet <factsbe0@gmail.com>
2024-05-03 22:18:13 +0530

    Headers in Route Handlers

--- route-handle-demo/src/app/dashboard/api/route.ts
+++ route-handle-demo/src/app/dashboard/api/route.ts
@@ -1,3 +1,14 @@
-export async function GET() {
-	return new Response("Dashboard API Documentation ");
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+	const requestHeader = new Headers(request.headers);
+	const headersList = headers();
+	console.log(requestHeader.get("Authorization"));
+	console.log(headersList.get("Authorization"));
+	return new Response("<h1>Dashboard API Documentation</h1>", {
+		headers: {
+			"Content-Type": "text/html",
+		},
+	});
 }
